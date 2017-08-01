e12779fd63550782f69737b110057dae704f1646
johang2012 <johan.gudmundsson2012@gmail.com>
2017-08-01 16:52:22 +0200

    Refactored to stateless pure component

--- src/client/components/LoginSlider/Login.jsx
+++ src/client/components/LoginSlider/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SliderContainer from './SliderContainer';
 import FormIcon from './FormIcon';
 import Button from '../Button';
@@ -6,23 +6,19 @@ import TextInput from '../TextInput';
 import SubTitle from '../SubTitle';
 import FlexBox from '../FlexBox';
 
-class Login extends Component {
-  render() {
-    return (
-      <SliderContainer>
-        <SubTitle uppercase primary>login</SubTitle>
-        <form>
-          <TextInput placeholder="email" />
-          <FormIcon icon="envelope" />
-          <TextInput placeholder="password" />
-          <FormIcon icon="lock" />
-          <FlexBox height="100px" justifyContent="flex-end" alignItems="flex-end">
-            <Button primary form>login</Button>
-          </FlexBox>
-        </form>
-      </SliderContainer>
-    );
-  }
-}
+const Login = () => (
+  <SliderContainer>
+    <SubTitle uppercase primary>login</SubTitle>
+    <form>
+      <TextInput placeholder="email" />
+      <FormIcon icon="envelope" />
+      <TextInput placeholder="password" />
+      <FormIcon icon="lock" />
+      <FlexBox height="100px" justifyContent="flex-end" alignItems="flex-end">
+        <Button primary form>login</Button>
+      </FlexBox>
+    </form>
+  </SliderContainer>
+);
 
 export default Login;
--- src/client/components/LoginSlider/Register.jsx
+++ src/client/components/LoginSlider/Register.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SubTitle from '../SubTitle/';
 import FormIcon from './FormIcon';
 import SliderContainer from './SliderContainer';
@@ -6,23 +6,19 @@ import TextInput from '../TextInput/';
 import Button from '../Button/';
 import FlexBox from '../FlexBox';
 
-class Register extends Component {
-  render() {
-    return (
-      <SliderContainer>
-        <SubTitle uppercase primary>sign up</SubTitle>
-        <form>
-          <TextInput placeholder="email" />
-          <FormIcon icon="envelope" />
-          <TextInput placeholder="password" />
-          <FormIcon icon="lock" />
-          <FlexBox height="100px" justifyContent="flex-end" alignItems="flex-end">
-            <Button primary form>sign up</Button>
-          </FlexBox>
-        </form>
-      </SliderContainer>
-    );
-  }
-}
+const Register = () => (
+  <SliderContainer>
+    <SubTitle uppercase primary>sign up</SubTitle>
+    <form>
+      <TextInput placeholder="email" />
+      <FormIcon icon="envelope" />
+      <TextInput placeholder="password" />
+      <FormIcon icon="lock" />
+      <FlexBox height="100px" justifyContent="flex-end" alignItems="flex-end">
+        <Button primary form>sign up</Button>
+      </FlexBox>
+    </form>
+  </SliderContainer>
+);
 
 export default Register;
