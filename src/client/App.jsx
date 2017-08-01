50b1716ba5e04910449dd406c971c5c63a63af58
johang2012 <johan.gudmundsson2012@gmail.com>
2017-08-01 16:29:12 +0200

    Refactored to pure stateless component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
 import LoginPage from './LoginPage';
 import Dashboard from './Dashboard';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/dashboard" component={Dashboard} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Route exact path="/" component={Home} />
+    <Route path="/login" component={LoginPage} />
+    <Route path="/dashboard" component={Dashboard} />
+  </div>
+);
 
 export default App;
