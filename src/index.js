ba85fef312adb48ab8acb758608a0558e80f5745
johang2012 <johan.gudmundsson2012@gmail.com>
2017-08-01 15:19:13 +0200

    Added Provider-component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import WebFont from 'webfontloader';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './stores/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+WebFont.load(
+  {
+    google: {
+      families: ['Poppins'],
+    },
+    custom: {
+      families: ['FontAwesome'],
+      urls: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'],
+    },
+  },
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
